343c1dbbef8077f160423060d4adce6258673e55
atagan <a.savas@hotmail.com.tr>
2022-04-21 18:26:26 +0300

    feat: styles removed & styles-components added

---
+++ src/styles/globalStyle.ts
@@ -0,0 +1,14 @@
+import { createGlobalStyle } from 'styled-components';
+
+export default createGlobalStyle`
+  *{
+    margin: 0;
+    padding: 0;
+    outline:0;
+  }
+  #root{
+    margin: 0;
+    padding: 0;
+    height: 100vh;
+  }
+ `;
