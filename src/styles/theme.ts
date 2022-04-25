7789cfaf6e2035b20328c3c4134be38255e5258a
atakan.savas <atakan.savas@kariyer.net>
2022-04-25 18:48:21 +0300

    feat: theme added

---
+++ src/styles/theme.ts
@@ -0,0 +1,15 @@
+import { DefaultTheme } from 'styled-components';
+
+const theme: DefaultTheme = {
+  borderRadius: '80px',
+
+  colors: {
+    WHITE: '#fff',
+    GREY_500: '#696969',
+    GRAY_100: '#E3E4E7',
+    BLUE_100: '#ADB9C3',
+    BLUE_500: '#3878A2',
+  },
+};
+
+export { theme };
