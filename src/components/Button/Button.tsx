e611087cb0a142bf78ebf619786943f10415abab
atakan.savas <atakan.savas@kariyer.net>
2022-04-25 15:41:05 +0300

    feat: button component props updated

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -2,15 +2,15 @@ import { memo } from 'react';
 import ButtonStyle from './button.styles';
 
 interface IButtonProps {
-  text: string;
+  children: JSX.Element | React.ReactChild | string;
   onClick?: React.MouseEventHandler<HTMLButtonElement>;
 }
 
 function Button(props: IButtonProps) {
-  const { text, onClick = () => {}, ...rest } = props;
+  const { children, onClick = () => {}, ...rest } = props;
   return (
-    <ButtonStyle onClick={props.onClick} {...rest}>
-      {text}
+    <ButtonStyle onClick={onClick} {...rest}>
+      {children}
     </ButtonStyle>
   );
 }
